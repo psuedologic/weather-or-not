af01ef5d8913bef2bc147d3adb4cbe21a87578f3
Talon Gonyeau <talongonyeau@gmail.com>
2020-08-13 15:19:17 -0700

    Cleaned up data request

--- weather-or-not/src/services/api.ts
+++ weather-or-not/src/services/api.ts
@@ -1,20 +1,26 @@
+import { timer, from, of } from "rxjs";
 import { fromFetch } from 'rxjs/fetch';
+
+import { mergeMap, takeUntil, tap, switchMap, map } from "rxjs/operators";
 
 export class ApiService {
     public API_KEY = '9de1aab5593fdc0061af9140d5e75eb8';
     public DEFAULT_CITY = 'London';
+    public myResponse: Response;
 
     constructor(){
-
         const data = 
-        fromFetch(`https://api.openweathermap.org/data/2.5/weather?q=${this.DEFAULT_CITY}&appid=${this.API_KEY}`).subscribe(
+        fromFetch(`https://api.openweathermap.org/data/2.5/weather?q=${this.DEFAULT_CITY}&appid=${this.API_KEY}`)
+        .pipe( map(response => response.json() ))
+        .subscribe(
             res => {
-                console.log("this is the response ",res); 
+                console.log("this is the data response ");
+                console.log(res)
+            },
+                err =>{
+                // Server is returning a status requiring the client to try something else.
+                return of({ error: true, message: `Error ${err.status}` });
             }
         );
-
     }
-
-
-
 }
